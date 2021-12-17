8fc519eed25d22c8ddf80d84bfb695bd2b350ffe
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-12-18 01:50:19 +0630

    feat(component): add Introduction page component

---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,61 @@
+import {Link, styled, Typography} from "@mui/material";
+import StyledSection from '../../styled-components/Section'
+import {useEffect, useRef} from "react";
+
+export default function Introduction() {
+    let section = useRef()
+
+    useEffect(() => {
+        let timeout = 200
+        section.current?.childNodes.forEach(child => {
+            setTimeout(() => {
+                child.classList.add('fade-in')
+            }, timeout)
+
+            timeout += 200
+        })
+    }, [])
+
+    return (
+        <Section ref={section}>
+            <Typography variant={'body1'} color={'text.hint'}>
+                Hi, my name is
+            </Typography>
+            <Typography variant={'body1'} color={'text.primary'}>
+                Chan Nyein Thaw
+            </Typography>
+            <Typography variant={'body1'} color={'text.secondary'}>
+                I build things for the web.
+            </Typography>
+            <Typography variant={'body1'} color={'text.secondary'}>
+                I’m a software engineer specializing in back-end development. Currently, I’m building APIs and web applications at &nbsp;
+                <Link variant={'body1'} color={'text.hint'} sx={{ textDecoration: 'underline'}}>One Terrace</Link>.
+            </Typography>
+        </Section>
+    )
+}
+
+let Section = styled(StyledSection)({
+    gridAutoFlow: 'row',
+    alignContent: 'center',
+
+    '& a': { textDecoration: 'none', cursor: 'pointer' },
+
+    '& > *': { opacity: 0 },
+
+    '& > *:nth-of-type(2)': {
+        fontWeight: 600,
+        fontSize: 'clamp(40px, 8vw, 80px)',
+        lineHeight: 1.1
+    },
+
+    '& > *:nth-of-type(3)': {
+        marginTop: 10,
+        fontWeight: 600,
+        fontSize: 'clamp(40px, 6vw, 80px)',
+        lineHeight: .9
+    },
+
+    '& > *:first-of-type': { marginLeft: 2, marginBottom: 30, fontFamily: '"Fira Code", sans-serif' },
+    '& > *:last-of-type': { marginTop: 20, maxWidth: 540 },
+})
