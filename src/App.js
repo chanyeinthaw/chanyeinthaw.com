7ea7f7cea26bb510251af6052294a809a2c629ba
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-12-18 01:50:40 +0630

    feat(app): render introduction section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Introduction from "./components/Introduction/Introduction";
+import {styled} from "@mui/material";
+
 function App() {
-    return <div />
+    return (
+        <Main>
+            <NavigationBar />
+            <Introduction />
+        </Main>
+    )
 }
+
+const Main = styled('main')({
+    display: 'flex',
+    flexDirection: 'column'
+})
 
 export default App;
