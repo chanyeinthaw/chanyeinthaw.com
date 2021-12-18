bcbfc0a09a10a7d0fb32a355e07a921355630752
Chan Nyein Thaw <me@chanyeinthaw.com>
2021-12-19 01:31:00 +0630

    fix(style): add 150 margin for screens > lg

--- src/styled-components/Section.js
+++ src/styled-components/Section.js
@@ -2,17 +2,21 @@ import {styled} from "@mui/material";
 
 const Section = styled('section')(props => ({
     padding: '0 150px',
+    margin: '0 150px',
 
     [props.theme.breakpoints.down('lg')]: {
-        padding: '0 100px'
+        padding: '0 100px',
+        margin: '0 0'
     },
 
     [props.theme.breakpoints.down('md')]: {
-        padding: '0 50px'
+        padding: '0 50px',
+        margin: '0 0'
     },
 
     [props.theme.breakpoints.down('sm')]: {
-        padding: '0 25px'
+        padding: '0 25px',
+        margin: '0 0'
     },
 }))
 
